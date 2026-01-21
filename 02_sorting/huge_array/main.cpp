801c5b5961adb109f6addffececaa90396867b44
Nat Silprasert <natsilprasert@gmail.com>
2026-01-21 13:27:20 +0700

    huge array

---
+++ 02_sorting/huge_array/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long N, Q;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> Q;
+    vector<pair<long long, long long>> sort_array(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> sort_array[i].first >> sort_array[i].second;
+    }
+    sort(sort_array.begin(), sort_array.end());
+
+    vector<long long> num_before(N);
+    num_before[0] = sort_array[0].second;
+    for (int i = 0; i < N - 1; i++)
+    {
+        num_before[i + 1] = num_before[i] + sort_array[i + 1].second;
+    }
+  
+    for (int i = 0; i < Q; i++)
+    {
+        long long input;
+        cin >> input;
+        long long real_idx = lower_bound(num_before.begin(), num_before.end(), input) - num_before.begin();
+        cout << sort_array[real_idx].first << endl;
+    }
+}
