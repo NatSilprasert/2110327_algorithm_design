a0ca8a1c6cc5da9ac9ead55333980a7e0bd6c0ca
Nat Silprasert <natsilprasert@gmail.com>
2026-02-08 23:32:00 +0700

    mcs

---
+++ 04_divide_conquer/05_maximum_subarray_sum/mcs/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+// get sum A[a..b] in O(1)
+ll get_sum(const vector<ll> &S, int a, int b)
+{
+    if (a == 0)
+        return S[b];
+    return S[b] - S[a - 1];
+}
+
+// Maximum Subarray Sum using Divide & Conquer + Prefix Sum
+ll mss(const vector<ll> &A,
+       const vector<ll> &S,
+       int start, int stop)
+{
+    // base case
+    if (start == stop)
+    {
+        return A[start];
+    }
+
+    int m = (start + stop) / 2;
+
+    // left and right
+    ll r1 = mss(A, S, start, m);
+    ll r2 = mss(A, S, m + 1, stop);
+
+    // find max of B[start..m][m]
+    ll max_sum_left = A[m];
+    for (int i = start; i <= m - 1; i++)
+    {
+        max_sum_left = max(max_sum_left, get_sum(S, i, m));
+    }
+
+    // find max of B[m+1..stop][stop]
+    ll max_sum_right = A[m + 1];
+    for (int j = m + 2; j <= stop; j++)
+    {
+        max_sum_right = max(max_sum_right, get_sum(S, m + 1, j));
+    }
+
+    ll r3 = max_sum_left + max_sum_right;
+
+    return max({r1, r2, r3});
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> A(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    // build prefix sum
+    vector<ll> S(n);
+    S[0] = A[0];
+    for (int i = 1; i < n; i++)
+    {
+        S[i] = S[i - 1] + A[i];
+    }
+
+    cout << mss(A, S, 0, n - 1) << endl;
+
+    return 0;
+}
