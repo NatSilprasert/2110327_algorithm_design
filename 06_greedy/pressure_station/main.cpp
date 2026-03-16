53a3f11c8d0eeb6995ea0d712ce9858e55ef5c0b
Nat Silprasert <natsilprasert@gmail.com>
2026-03-16 10:10:49 +0700

    pressure station

---
+++ 06_greedy/pressure_station/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> cost, best;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    cost.resize(n);
+    best.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> cost[i];
+    }
+    for (int i = 0; i <= std::min(n - 1, k); i++) {
+        best[i] = cost[i];
+    }
+
+    int start = k + 1;
+    int stop = n - 1;
+    for (int i = start; i <= stop; i++) {
+        best[i] = cost[i] + best[i - 1];
+        for (int j = 1; j <= 2 * k + 1; j++) {
+            if (i - j < 0) break;
+            if (cost[i] + best[i - j] < best[i]) best[i] = cost[i] + best[i - j];
+        }
+    }
+  
+    int ans = best[n - 1];
+    for (int i = n - 1; i >= std::max(0, n - k - 1); i--) {
+        if (best[i] < ans) ans = best[i];
+    }
+    
+    cout << ans << endl;
+}
